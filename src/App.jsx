e32575f7910c7be3f53a5c5119b5279359877510
s3xmaxman <badside3919@gmail.com>
2024-09-25 18:30:11 +0900

    build(deps): update package-lock.json, package.json, App.jsx and src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { CylinderGeometry, DirectionalLight, MeshLambertMaterial } from "three";
 
-function App() {
-  const [count, setCount] = useState(0)
+const RotatingCube = () => {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <mesh ref={meshRef}>
+      <CylinderGeometry args={[1, 1, 1, 32]} />
+      <MeshLambertMaterial color="#468585" emissive="#468585" />
+    </mesh>
+  );
+};
 
-export default App
+const App = () => {
+  return (
+    <Canvas
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <OrbitControls enableZoom enablePan enableRotate />
+      <DirectionalLight position={[1, 1, 1]} intensity={10} color={0x9cdba6} />
+      {/* <color attach="background" args={["#F0F0F0"]} /> */}
+      <RotatingCube />
+    </Canvas>
+  );
+};
+
+export default App;

c23ae409f9836c415464960e724cf2381d20d20f
s3xmaxman <badside3919@gmail.com>
2024-09-26 02:27:57 +0900

    update eslint.config.js and Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,20 @@
+import { Leva } from "leva";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { useMediaQuery } from "react-responsive";
+import { PerspectiveCamera } from "@react-three/drei";
+
+import { calculateSizes } from "../constants";
+import { HackerRoom } from "../components/HackerRoom";
+import CanvasLoader from "../components/Loading";
+
 const Hero = () => {
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -10,7 +26,21 @@ const Hero = () => {
         </p>
       </div>
 
-      <div className="w-full h-full absolute inset-0"></div>
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom
+              scale={sizes.deskScale}
+              position={sizes.deskPosition}
+              rotation={[0.1, -Math.PI, 0]}
+            />
+            <Leva hidden />
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
     </section>
   );
 };

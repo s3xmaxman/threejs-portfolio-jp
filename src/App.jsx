e41479d62740df50b81ab3d8620ea20f0f89d94d
s3xmaxman <badside3919@gmail.com>
2024-09-25 18:47:14 +0900

    update vite.svg and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,10 @@
-import { OrbitControls } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useRef } from "react";
-import { CylinderGeometry, DirectionalLight, MeshLambertMaterial } from "three";
-
-const RotatingCube = () => {
-  const meshRef = useRef();
-
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.y += 0.01;
-    }
-  });
-
-  return (
-    <mesh ref={meshRef}>
-      <CylinderGeometry args={[1, 1, 1, 32]} />
-      <MeshLambertMaterial color="#468585" emissive="#468585" />
-    </mesh>
-  );
-};
+import Navbar from "./sections/Navbar";
 
 const App = () => {
   return (
-    <Canvas
-      style={{
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <OrbitControls enableZoom enablePan enableRotate />
-      <DirectionalLight position={[1, 1, 1]} intensity={10} color={0x9cdba6} />
-      {/* <color attach="background" args={["#F0F0F0"]} /> */}
-      <RotatingCube />
-    </Canvas>
+    <main className="max-w-7xl mx-auto relative">
+      <Navbar />
+    </main>
   );
 };
 

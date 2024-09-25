d92ceba7b5765246e7f5fbf841bce18c4662279b
s3xmaxman <badside3919@gmail.com>
2024-09-26 00:17:53 +0900

    build(deps): update package-lock.json, package.json and Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,18 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Hi, I am S3X <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Building Products & Brands
+        </p>
+      </div>
+
+      <div className="w-full h-full absolute inset-0"></div>
+    </section>
+  );
 };
 
 export default Hero;

1a850b2cd98b3010d19fae99f30331fa095e6282
s3xmaxman <badside3919@gmail.com>
2024-09-27 06:59:07 +0900

    update App.jsx

---
+++ src/sections/About.jsx
@@ -0,0 +1,140 @@
+import { useState } from "react";
+import Globe from "react-globe.gl";
+
+import Button from "../components/Button.jsx";
+
+const About = () => {
+  const [hasCopied, setHasCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText("adrian@jsmastery.pro");
+    setHasCopied(true);
+
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000);
+  };
+
+  return (
+    <section className="c-space my-20" id="about">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="assets/grid1.png"
+              alt="grid-1"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
+
+            <div>
+              <p className="grid-headtext">こんにちは、Badです</p>
+              <p className="grid-subtext">
+                12年間の経験で、フロントエンドとバックエンド両方の開発スキルを磨き、動的でレスポンシブなウェブサイトを作成してきました。
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="assets/grid2.png"
+              alt="grid-2"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
+
+            <div>
+              <p className="grid-headtext">技術スタック</p>
+              <p className="grid-subtext">
+                堅牢でスケーラブルなアプリケーションを構築できる様々な言語、フレームワーク、ツールを専門としています。
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0, 0, 0, 0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                labelsData={[
+                  {
+                    lat: 40,
+                    lng: -100,
+                    text: "リエカ、クロアチア",
+                    color: "white",
+                    size: 15,
+                  },
+                ]}
+              />
+            </div>
+            <div>
+              <p className="grid-headtext">
+                タイムゾーンのコミュニケーションと場所に関して、とても柔軟に対応できます。
+              </p>
+              <p className="grid-subtext">
+                クロアチアのリエカを拠点としており、世界中どこでもリモートワークに対応可能です。
+              </p>
+              <Button
+                name="お問い合わせ"
+                isBeam
+                containerClass="w-full mt-10"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="assets/grid3.png"
+              alt="grid-3"
+              className="w-full sm:h-[266px] h-fit object-contain"
+            />
+
+            <div>
+              <p className="grid-headtext">コーディングへの情熱</p>
+              <p className="grid-subtext">
+                問題を解決し、コードで何かを構築することが大好きです。
+                プログラミングはただの職業ではなく、私の情熱です。
+                新しい技術を探求し、スキルを向上させることを楽しんでいます。
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">連絡先</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img
+                  src={hasCopied ? "assets/tick.svg" : "assets/copy.svg"}
+                  alt="copy"
+                />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">
+                  adrian@jsmastery.pro
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;

4a90f9bc2c33cd12de8d24d12e1e59448ddf99f3
s3xmaxman <badside3919@gmail.com>
2024-09-29 21:49:09 +0900

    update App.jsx

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,89 @@
+import { useRef, useState } from "react";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = () => {};
+
+  const handleSubmit = () => {};
+
+  return (
+    <section className="c-space my-20">
+      <div className="relative min-h-screen flex items-center justify-center flex-col">
+        <img
+          src="/assets/terminal.png"
+          alt="terminal-bg"
+          className="absolute inset-0 min-h-screen"
+        />
+        <div className="contact-container">
+          <h3 className="head-text">Let&#39;s talk</h3>
+          <p className="text-lg text-white-600 mt-3">
+            Whether you’re looking to build a new website, improve your existing
+            platform, or bring a unique project to life, I’m here to help.
+          </p>
+
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className="mt-12 flex flex-col space-y-7"
+          >
+            <label className="space-y-3">
+              <span className="field-label">Full Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                className="field-input"
+                onChange={handleChange}
+                required
+                placeholder="Enter your name"
+              />
+            </label>
+
+            <label className="space-y-3">
+              <span className="field-label">Email</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                className="field-input"
+                onChange={handleChange}
+                required
+                placeholder="Enter your email"
+              />
+            </label>
+
+            <label className="space-y-3">
+              <span className="field-label">Message</span>
+              <textarea
+                name="message"
+                value={form.message}
+                className="field-input"
+                onChange={handleChange}
+                required
+                placeholder="Enter your message"
+              />
+            </label>
+
+            <button className="field-btn" type="submit" disabled={loading}>
+              {loading ? "Sending..." : "Send Message"}
+              <img
+                src="/assets/arrow-up.png"
+                alt="arrow-up"
+                className="field-btn_arrow"
+              />
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;

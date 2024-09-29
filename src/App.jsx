60da7c90b7337964669608854e0538b7c874d566
s3xmaxman <badside3919@gmail.com>
2024-09-29 22:48:38 +0900

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./sections/About";
 import Clients from "./sections/Clients";
 import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import Projects from "./sections/Projects";
@@ -14,6 +15,7 @@ const App = () => {
       <Projects />
       <Clients />
       <Contact />
+      <Footer />
     </main>
   );
 };
